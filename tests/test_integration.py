56d40ccaafd06ba761786d996afe8dba01d4917d
Pior Bastida <pior@pbastida.net>
2018-03-30 23:48:19 -0400

    Add integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,6 +6,7 @@ import pytest
 def git(path, *args):
     process = run(['git', '-C', path, *args], stdout=PIPE, stderr=PIPE)
     assert process.returncode == 0
+    return process
 
 
 @pytest.fixture
@@ -21,22 +22,71 @@ def repo(tmpdir):
     upstream = tmpdir.mkdir('upstream')
     git(upstream, 'init')
 
+    upstream.join('afile').write('value-1')
+    git(upstream, 'add', 'afile')
+    git(upstream, 'commit', '-m', 'added afile')
+
+    git(upstream, 'tag', 'v0.1')
+
     repo = tmpdir.mkdir('repo')
     git(tmpdir, 'clone', upstream, repo)
 
-    repo.join('afile').write('afile-content')
-    git(repo, 'add', 'afile')
-    git(repo, 'commit', '-m', 'added afile')
+    repo.join('afile').write('value-2')
+    git(repo, 'commit', 'afile', '-m', 'update afile')
+
+    git(upstream, 'tag', 'v0.2')
+
     return repo
+
+
+SETUPPY = """
+from setuptools import setup
+
+VERSION = '0.0'
+
+setup(
+    name='test',
+    version=VERSION,
+    author='supercat',
+    author_email='supercat@cat.cat',
+    url='https://cat.cat',
+)
+"""
+
+
+@pytest.fixture
+def project(repo):
+    repo.join('setup.py').write(SETUPPY)
+    repo.join('README').write('Read me.')
 
 
 def test_usage(cmd):
     proc = cmd(stdout=PIPE, stderr=PIPE)
-    assert proc.returncode == 2
     assert b'arguments are required' in proc.stderr
 
 
-def test_poi(cmd, repo):
+def test_no_setuppy(cmd, repo):
     proc = cmd('1.0', stdout=PIPE, stderr=PIPE)
-    assert proc.returncode == 2
-    assert b'arguments are required' in proc.stderr
+    assert b'missing setup.py file' in proc.stderr
+
+
+def test_local_tag_exists(cmd, repo):
+    proc = cmd('0.1', stdout=PIPE, stderr=PIPE)
+    assert b'tag already exists locally' in proc.stderr
+
+
+def test_remote_tag_exists(cmd, repo):
+    proc = cmd('0.2', stdout=PIPE, stderr=PIPE)
+    assert b'tag already exists remotely' in proc.stderr
+
+
+def test_run(cmd, repo, project):
+    proc = cmd('1.0', stdout=PIPE, stderr=PIPE)
+    assert proc.returncode == 0
+    assert b'' == proc.stderr
+
+    setuppy = repo.join('setup.py').read()
+    assert "\nVERSION = '1.0'  # " in setuppy
+
+    tags = git(repo, 'tag').stdout.split(b'\n')
+    assert b'v1.0' in tags
